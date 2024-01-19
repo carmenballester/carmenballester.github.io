db95300442d1d6bbac924c7380c485a5c9a57c95
Carmen Ballester Bernabeu <carmenballesterbernabeu@Carmens-MacBook-Air.local>
2024-01-19 16:29:31 +0100

    nav menu major improvements

---
+++ scripts/activePage.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentLocation = window.location.pathname; // Get only the path part of the URL
+    const menuButtons = document.querySelectorAll('.menu_button a');
+
+    menuButtons.forEach(function(button) {
+        const buttonPath = new URL(button.href).pathname; // Extract path from button's href
+        if (currentLocation === buttonPath) {
+            button.parentElement.classList.add('active');
+        }
+    });
+});
