b486239280deda452dff8cb7f1d22d240103ac83
Carmen Ballester Bernabeu <carmenballesterbernabeu@Carmens-MacBook-Air.local>
2024-02-08 13:45:21 +0100

    intro 100% responsive

--- scripts/resize.js
+++ scripts/resize.js
@@ -2,6 +2,7 @@ window.addEventListener('resize', function() {
     var rectangle = document.querySelector('.title-rectangle');
     var text = document.querySelector('.title-text');
     var viewportWidth = window.innerWidth;
+    var img = document.querySelector('.picture-wrapper img');
 
     if (viewportWidth >= 620) {
         rectangle.style.top = (viewportWidth*0.16) + 'px';
@@ -16,6 +17,9 @@ window.addEventListener('resize', function() {
 
         text.style.fontSize = (viewportWidth*0.08) + 'px';
     }
+
+    img.style.borderRadius = (viewportWidth*0.02) + 'px';
+    
 
   });
 
