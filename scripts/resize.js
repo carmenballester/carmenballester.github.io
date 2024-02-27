b5006a551ea1861abdf053be14194ae3ce5a1ffa
Carmen Ballester Bernabeu <carmenballesterbernabeu@Carmens-MacBook-Air.local>
2024-02-27 21:06:22 +0100

    easter-egg

--- scripts/resize.js
+++ scripts/resize.js
@@ -5,21 +5,28 @@ window.addEventListener('resize', function() {
     var viewportWidth = window.innerWidth;
     var img = document.querySelector('.picture-wrapper img');
 
-    if (viewportWidth >= 620) {
-        rectangle.style.top = (viewportWidth*0.16) + 'px';
+    if (rectangle !== null) {
+        if (viewportWidth >= 620) {
+            rectangle.style.top = (viewportWidth * 0.16) + 'px';
+        } else {
+            rectangle.style.top = viewportWidth * 0.14 + 'px';
+        }
+        rectangle.style.height = (viewportWidth / 16) + 'px';
     }
 
-    else {
-        rectangle.style.top = viewportWidth*0.14 + 'px';
+    if (img !== null) {
+        img.style.borderRadius = (viewportWidth * 0.02) + 'px';
     }
 
-    title.style.fontSize = (viewportWidth*0.08) + 'px';
-    subtitle.style.fontSize = 16 + viewportWidth*0.01 + 'px';
-    rectangle.style.height = (viewportWidth/16) + 'px';
-    img.style.borderRadius = (viewportWidth*0.02) + 'px';
-    
+    if (title !== null) {
+        title.style.fontSize = (viewportWidth * 0.08) + 'px';
+    }
 
-  });
+    if (subtitle !== null) {
+        subtitle.style.fontSize = 16 + viewportWidth * 0.01 + 'px';
+    }
+
+});
 
 // Initial positioning when the page loads
 window.dispatchEvent(new Event('resize'));
